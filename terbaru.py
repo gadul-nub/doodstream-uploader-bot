8b223a9d9b4820c40db1c038961e7adca70480d8
gadul-nub <asep.galagadul@gmail.com>
2025-05-27 18:42:38 +0700

    Add files via upload

---
+++ terbaru.py
@@ -0,0 +1,59 @@
+from pyrogram import Client, filters
+import requests
+import os
+
+API_ID ="11757512"
+API_HASH = "19142777e9ee3cd8809a30107323c84b"
+BOT_TOKEN ="8160463425:AAGyXQ99qTTEI8yt_kr0BVDBiPJu8TngUJs"
+
+bot = Client("dood_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+user_api_keys = {} 
+
+@bot.on_message(filters.command("start") & filters.private)
+async def start(bot, message):
+    await message.reply("👋 Halo! {message.chat.first_name}\nKirim `/api your_doodstream_api_key` untuk mulai.")
+
+@bot.on_message(filters.command("api") & filters.private)
+async def set_api(bot, message):
+    try:
+        api_key = message.text.split(" ", 1)[1]
+        user_api_keys[message.from_user.id] = api_key
+        await message.reply("✅ API key disimpan.")
+    except IndexError:
+        await message.reply("⚠️ Format salah. Gunakan: `/api your_api_key`")
+
+@bot.on_message((filters.video | filters.document) & filters.private)
+async def handle_video(bot, message):
+    user_id = message.from_user.id
+    api_key = user_api_keys.get(user_id)
+
+    if not api_key:
+        return await message.reply("❗ API key belum diset. Gunakan `/api your_key`.")
+
+    await message.reply("📥 Mengunduh video...")
+    file_path = await message.download()
+    
+    try:
+        server = requests.get(f"https://doodapi.co/api/upload/server?key={api_key}").json()
+        upload_url = server["result"]
+
+        await message.reply("⏫ Mengunggah ke DoodStream...")
+        with open(file_path, 'rb') as f:
+            res = requests.post(f"{upload_url}?{api_key}", files={'file': f}, data={'api_key': api_key})
+        result = res.json()
+
+        if result.get("status") == 200:
+            msg = result["msg"]
+            time = result["server_time"]
+            total =result["total_slots"]
+            used =result["used_slots"]
+            await message.reply(f"✅ Sukses!\n📎 Pesan: {msg}\nWaktu server: {time}\nTotal: {total}\nDigunakan: {used}")
+        else:
+            await message.reply(f"❌ Gagal upload:\n {result}")
+    except Exception as e:
+        await message.reply(f"❌ Terjadi kesalahan:\n {e}")
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+bot.run()
